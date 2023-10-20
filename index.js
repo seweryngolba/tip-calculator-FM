bd71a0d400503a94b7b60c84a940d104c2ac6289
Seweryn Golba <sgoolb@gmail.com>
2023-10-20 22:50:14 +0200

    js selectors

---
+++ index.js
@@ -0,0 +1,6 @@
+const resetBtn = document.querySelector(".reset-button");
+const moneyInput = document.querySelector(".bill-input");
+const peopleInput = document.querySelector(".people-input");
+const customTip = document.querySelector(".custom-input");
+const tipDisplay = document.querySelector(".calculated-amount");
+const totalDisplay = document.querySelector(".calculated-display");
