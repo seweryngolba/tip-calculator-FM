54530465ca05ea6b5e4c873a1184e691e1f7fdfa
Seweryn Golba <sgoolb@gmail.com>
2023-10-24 21:29:21 +0200

    choosing tip js

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const peopleInput = document.querySelector(".people-input");
 const customTip = document.querySelector(".custom-input");
 const tipDisplay = document.querySelector(".calculated-amount");
 const totalDisplay = document.querySelector(".calculated-display");
+const tips = document.querySelectorAll(".tip");
+
+tips.forEach((tip) => {
+  tip.addEventListener("click", () => {
+    tips.forEach((item) => item.classList.remove("active"));
+    tip.classList.add("active");
+  });
+});
